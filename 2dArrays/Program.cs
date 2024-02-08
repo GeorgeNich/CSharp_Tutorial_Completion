739f23825450a9b954a73bf4672edfd023c0b65d
George <80371942+GeorgeNich@users.noreply.github.com>
2024-02-08 23:39:59 +1100

    Adding C# source code files and project file for 2D arrays example.

---
+++ 2dArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2dArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid = { 
+                {1, 2},
+                {3, 4},
+                {5, 6}
+            };
+            //int[,] myArray = new int[2, 3];
+
+            Console.WriteLine(numberGrid[2, 1]);
+            
+            Console.ReadLine();
+        }
+    }
+}
